2a035f5cda5577d71b8aeaedd36b72cf6d066455
Getqo <getqo.az@gmail.com>
2025-03-16 01:27:20 +0400

    Update getAddressFromLocation.ts

--- utils/getAddressFromLocation.ts
+++ utils/getAddressFromLocation.ts
@@ -2,11 +2,16 @@ import axios from "axios";
 import { MAP_API_KEY } from "constants/constants";
 
 export async function getAddressFromLocation(latlng?: string) {
-  let params = { latlng, key: MAP_API_KEY };
+  let params = { 
+    latlng, 
+    key: MAP_API_KEY, 
+    region: "AZ", // Azerbaycan ile sınırla
+    language: "az" // Azerbaycan Türkçesi
+  };
 
   return axios
     .get(`https://maps.googleapis.com/maps/api/geocode/json`, { params })
-    .then(({ data }) => data.results[0]?.formatted_address)
+    .then(({ data }) => data.results[0]?.formatted_address || "not found")
     .catch((error) => {
       console.log(error);
       return "not found";
